96e0d2a513b7623466b26af5d6bc36d1472b541e
Amanda <kraniasar@gmail.com>
2016-03-24 21:26:36 -0400

    implementing devise and implementing bootswatch

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   get 'welcome/index'
 
   get 'welcome/about'
 
   devise_for :users
 
-  root to: "home#index"
-
+  root to: "welcome#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
